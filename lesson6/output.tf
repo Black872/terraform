694eeb8d0d5fcfc4f7cbd1417ae1c69d31c5d9e4
Dark Work <dark@gmail.com>
2023-07-17 16:11:58 +0300

    Lesson 6 Output

---
+++ lesson6/output.tf
@@ -0,0 +1,18 @@
+#output tf file
+#output instance id
+output "webserver_instance_id" {
+  value = aws_instance.webserver6.id
+}
+#output public ip
+output "webserver_public_ip_address" {
+  value = aws_eip.static_ip.public_ip
+}
+#output security group id
+output "webserver_sg_id" {
+  value = aws_security_group.webserver6_sec.id
+}
+#output security group arn
+output "webserver_sg_arn" {
+  value       = aws_security_group.webserver6_sec.arn
+  description = "this is security arn"
+}
